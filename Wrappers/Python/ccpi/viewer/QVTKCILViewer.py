7b0d305ed17b9c98a085356a0fd156cd717fb668
Edoardo Pasca <edo.paskino@gmail.com>
2018-02-05 12:04:28 +0000

    code refactoring for version 0.9.1 (#10)
    closes #9

--- Wrappers/Python/ccpi/viewer/QVTKCILViewer.py
+++ Wrappers/Python/ccpi/viewer/QVTKCILViewer.py
@@ -1,3 +1,16 @@
+#   Copyright 2017 Edoardo Pasca
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 """
 QVTKCILViewer
 
@@ -42,6 +55,9 @@ Changes by Alex Tsui, Apr. 2015
 
 Changes by Fabian Wenzel, Jan. 2016
  Support for Python3
+ 
+Changes by Edoardo Pasca, 2017
+ Add picking for shift
 """
 
 # Check whether a specific PyQt implementation was chosen
