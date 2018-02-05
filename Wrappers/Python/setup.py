ba5e58825aae52c0268b493abb8d05e64d0de96e
Edoardo Pasca <edo.paskino@gmail.com>
2018-02-05 12:02:37 +0000

    code refactoring for version 0.9.1
    closes #9

--- Wrappers/Python/setup.py
+++ Wrappers/Python/setup.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
+#   Copyright 2017 Edoardo Pasca
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License."""
 Created on Wed Jun  7 09:57:13 2017
 
 @author: ofn77899
@@ -8,6 +20,7 @@ Created on Wed Jun  7 09:57:13 2017
 from distutils.core import setup
 #from setuptools import setup, find_packages
 import os
+import sys
 
 cil_version=os.environ['CIL_VERSION']
 if  cil_version == '':
@@ -18,7 +31,7 @@ setup(
     name="ccpi-viewer",
     version=cil_version,
     packages=['ccpi','ccpi.viewer'],
-	install_requires=['numpy','vtk'],
+	 install_requires=['numpy','vtk'],
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
