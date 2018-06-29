0d563645972f38554f9a62c1a4522985fbb344f2
rsmith013 <richard.d.smith@stfc.ac.uk>
2018-06-29 11:52:21 +0100

    Added myself to contributors

--- Wrappers/Python/ccpi/viewer/CILViewer.py
+++ Wrappers/Python/ccpi/viewer/CILViewer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 #   Copyright 2017 Edoardo Pasca
+#   Copyright 2018 Richard Smith
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
