d050c3ae119ce0d24891930a7c35626c6750e6d6
Edoardo Pasca <edo.paskino@gmail.com>
2020-03-23 11:58:07 +0000

    updated build rule for win

--- Wrappers/Python/conda-recipe/bld.bat
+++ Wrappers/Python/conda-recipe/bld.bat
@@ -5,7 +5,7 @@ exit 1
 
 xcopy /e "%RECIPE_DIR%\.." "%SRC_DIR%"
 
-%PYTHON% setup.py -q bdist_egg
+:: %PYTHON% setup.py -q bdist_egg
 :: %PYTHON% setup.py install --single-version-externally-managed --record=record.txt
 %PYTHON% setup.py install
 if errorlevel 1 exit 1
