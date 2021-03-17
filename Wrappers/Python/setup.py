82df31c5c52033521575ae236ef167933874432b
Edoardo Pasca <edo.pakino@gmail.com>
2021-03-17 14:11:16 +0000

    bump version to 21.0.0

--- Wrappers/Python/setup.py
+++ Wrappers/Python/setup.py
@@ -12,37 +12,23 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-"""
-Created on Wed Jun  7 09:57:13 2017
-
-@author: ofn77899
-"""
 
 from distutils.core import setup
-#from setuptools import setup, find_packages
 import os
 import sys
 
-cil_version = "21.0"
+cil_version = "21.0.0"
+if os.environ.get('CONDA_BUILD', None) is not None:
+    requires = []
+else:
+    requires = ['numpy','vtk']
 
 setup(
     name="ccpi-viewer",
     version=cil_version,
     packages=['ccpi','ccpi.viewer', 'ccpi.viewer.utils'],
-	install_requires=['numpy','vtk'],
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    #install_requires=['docutils>=0.3'],
-
-#    package_data={
-#        # If any package contains *.txt or *.rst files, include them:
-#        '': ['*.txt', '*.rst'],
-#        # And include any *.msg files found in the 'hello' package, too:
-#        'hello': ['*.msg'],
-#    },
-     zip_safe = False,
-
+	install_requires=requires,
+    zip_safe = False,
     # metadata for upload to PyPI
     author="Edoardo Pasca",
     author_email="edo.paskino@gmail.com",
