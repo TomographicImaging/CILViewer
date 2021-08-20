e0691e2683224771509de0f7237099378f98c077
Edoardo Pasca <edo.paskino@gmail.com>
2021-08-20 10:41:38 +0200

    update __version__ string from repo tag

--- Wrappers/Python/ccpi/viewer/__init__.py
+++ Wrappers/Python/ccpi/viewer/__init__.py
@@ -2,5 +2,6 @@ from .CILViewer import CILViewer as viewer3D
 from .CILViewer2D import CILViewer2D as viewer2D
 from .CILViewer import CILInteractorStyle as istyle3D
 from .CILViewer2D import CILInteractorStyle as istyle2D
+from .version import dversion
 
-__version__ = "21.1.0"
+__version__ = dversion
