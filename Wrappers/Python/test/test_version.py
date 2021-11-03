59f811f8a633df1662283e7fab32334afe4a3faf
Laura <60604372+lauramurgatroyd@users.noreply.github.com>
2021-11-03 17:37:18 +0000

    Add basic unit test for version (#150)
    * Add test for version
    
    * Run unit tests
    
    * set test source file path
    
    * Add -v to get more info about tests
    
    * Fix typo
    
    * "test" folder was written twice in paths
    
    * update path to test
    
    * Update meta.yaml
    
    * Update meta.yaml
    
    * change base dir for tests
    
    * update test dir
    
    * update test command
    
    * test action branch where libgl is installed
    
    * Add importlib_metadata
    
    * Create version.py
    
    * Remove creating version.py from setup.py
    
    * Update setup.py
    
    * Update to use v1.4.2 of conda-package-publish-action
    
    * Delete version.py
    
    * prints for testing
    
    * Update test_version.py
    
    * update to tests and make package noarch
    
    * add back importlib for no reason
    
    * fix test if env variabl CONDA_BUILD is 1
    
    * remove ls
    
    * Update build recipes inline with brem
    
    * Update conda build scripts in line with brem
    
    * Fix merge of action file
    
    * Fix merge of setup.py
    
    * Revert changes to bld.bat as it worked before
    
    * remove print
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    Co-authored-by: Edoardo Pasca <edo.paskino@gmail.com>

---
+++ Wrappers/Python/test/test_version.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestModuleBase(unittest.TestCase):
+    def test_version(self):
+        try:
+            from ccpi.viewer import version as dversion
+            a = dversion.version
+            print ("version", a)
+            self.assertTrue(isinstance(a, str))
+        except ImportError as ie:
+            self.assertFalse(True, str(ie))
