03b2086c301a01740b87a07d9800fe58bcb7f3a0
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2022-01-07 09:01:39 +0000

    Upon image selection, may create dialogs for raw image attributes or hdf5 attributes

--- Wrappers/Python/ccpi/viewer/viewer_main_window.py
+++ Wrappers/Python/ccpi/viewer/viewer_main_window.py
@@ -1,15 +1,7 @@
-import sys
-import vtk
-from PySide2 import QtCore, QtWidgets
-from ccpi.viewer import viewer2D, viewer3D
-from ccpi.viewer.QCILViewerWidget import QCILViewerWidget
-import ccpi.viewer.viewerLinker as vlink
-from ccpi.viewer.utils.conversion import Converter
-import numpy as np
-
 import json
 import os
 import shutil
+import sys
 import tempfile
 import time
 import zipfile
@@ -20,39 +12,33 @@ from pathlib import Path
 # Import linking class to join 2D and 3D viewers
 import ccpi.viewer.viewerLinker as vlink
 import h5py
+import numpy as np
 import qdarkstyle
 import vtk
 from ccpi.viewer import viewer2D, viewer3D
 from ccpi.viewer.iviewer import SingleViewerCenterWidget
-from ccpi.viewer.QCILViewerWidget import QCILDockableWidget
+from ccpi.viewer.QCILViewerWidget import QCILDockableWidget, QCILViewerWidget
 # from gui.settings_dialog import create_settings_dialog
 from ccpi.viewer.utils import Converter, cilPlaneClipper
+from ccpi.viewer.utils.conversion import Converter
 # , QCILViewerWidget
 from ccpi.viewer.utils.hdf5_io import HDF5Reader
 from eqt.threading import Worker
-from eqt.ui import FormDialog
+from eqt.threading.QtThreading import ErrorObserver
+from eqt.ui import FormDialog, UIFormFactory
 from eqt.ui.UIFormWidget import UIFormFactory
+from eqt.ui.UIMainWindow import MainWindow, SettingsDialog
 from eqt.ui.UIStackedWidget import StackedWidgetFactory
 # from eqt.ui.UIFormWidget import UIFormFactory
-from PySide2 import QtCore
+from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import QRegExp, QSettings, Qt, QThreadPool
 from PySide2.QtGui import QCloseEvent, QKeySequence, QRegExpValidator
-from PySide2.QtWidgets import (QAction,  QApplication, QCheckBox, QComboBox, QDockWidget,
-                               QFileDialog, QLabel, QLineEdit, QMainWindow,
-                               QMenu, QMessageBox, QProgressDialog,
-                               QPushButton, QSpinBox, QStackedWidget,
-                               QTabWidget)
-
-from eqt.ui.UIMainWindow import MainWindow, SettingsDialog
-
-from eqt.ui import  UIFormFactory
-from PySide2.QtWidgets import (QCheckBox, QDoubleSpinBox, QLabel, QDialog, QDialogButtonBox)
-
-from PySide2.QtCore import Qt
-
-from eqt.ui import UIFormFactory   
-
-from eqt.threading.QtThreading import ErrorObserver
+from PySide2.QtWidgets import (QAction, QApplication, QCheckBox, QComboBox,
+                               QDialog, QDialogButtonBox, QDockWidget,
+                               QDoubleSpinBox, QFileDialog, QLabel, QLineEdit,
+                               QMainWindow, QMenu, QMessageBox,
+                               QProgressDialog, QPushButton, QSpinBox,
+                               QStackedWidget, QTabWidget)
 
 #from ccpi.viewer.standalone_viewer import TwoLinkedViewersCenterWidget, SingleViewerCenterWidget
 
@@ -62,47 +48,23 @@ from eqt.threading.QtThreading import ErrorObserver
 
 class ViewerMainWindow(MainWindow):
 
-    def __init__(self, title="", viewer2_type = None, *args, **kwargs): #viewer1_type
+    def __init__(self, title="", viewer2_type=None, *args, **kwargs):  # viewer1_type
         MainWindow.__init__(self, title)
 
         # Note: does not create a viewer as we don't know whether the user would like it to exist in a
         # dockwidget or central widget
-        
-        # self.setUp(viewer1_type, viewer2_type, *args, **kwargs)
-        # self.show()
-        self.e = ErrorObserver() # only needed with old io module
-
+        # Instead it creates a main window with many of the tools needed for a window
+        # that will house a viewer.
+        self.e = ErrorObserver()  # only needed with old io module
 
         self.CreateViewerSettingsPanel()
         # could get rid of this:
-        self.addDockWidget(QtCore.Qt.BottomDockWidgetArea, self.viewer_settings_dock)
+        self.addDockWidget(QtCore.Qt.BottomDockWidgetArea,
+                           self.viewer_settings_dock)
 
         self.viewer_2D = None
 
         print("The settings: ", self.settings, self.settings.allKeys())
-
-    # def setUp(self, viewer1_type, viewer2_type = None, *args, **kwargs):
-    #     '''
-    #     viewer1_type: '2D' or '3D'
-    #     viewer2_type: '2D', '3D' or None - if None, only one viewer is displayed
-    #     '''
-
-    #     if viewer1_type == '2D':
-    #         viewer1 = viewer2D
-    #     elif viewer1_type == '3D':
-    #         viewer1 = viewer3D
-
-    #     if viewer2_type is None:
-    #         window = SingleViewerCenterWidget(viewer=viewer1)        
-    #     else:
-    #         if viewer2_type == '2D':
-    #             viewer2 = viewer2D
-    #         elif viewer2_type == '3D':
-    #             viewer2 = viewer3D
-
-    #         window = TwoLinkedViewersCenterWidget(viewer1=viewer1, viewer2=viewer2)
-
-    #     self.SetCe
 
 
     def open_settings_dialog(self):
@@ -116,22 +78,24 @@ class ViewerMainWindow(MainWindow):
         if hasattr(self, 'copy_files'):
             sw['copy_files_checkbox_field'].setChecked(self.copy_files)
         if self.settings.value("vis_size") is not None:
-            sw['vis_size_field'].setValue(float(self.settings.value("vis_size")))
+            sw['vis_size_field'].setValue(
+                float(self.settings.value("vis_size")))
         else:
             sw['vis_size_field'].setValue(1.0)
 
         if self.settings.value("gpu_size") is not None:
-            sw['gpu_size_field'].setValue(float(self.settings.value("gpu_size")))
+            sw['gpu_size_field'].setValue(
+                float(self.settings.value("gpu_size")))
         else:
             sw['gpu_size_field'].setValue(1.0)
-        
 
     def settings_dialog_accept(self, settings_dialog):
         MainWindow.settings_dialog_accept(self, settings_dialog)
 
         if settings_dialog.fw.widgets['copy_files_checkbox_field'].isChecked():
-            self.copy_files = 1 # save for this session
-            self.settings.setValue("copy_files", 1) #save for next time we open app
+            self.copy_files = 1  # save for this session
+            # save for next time we open app
+            self.settings.setValue("copy_files", 1)
         else:
             self.copy_files = 0
             self.settings.setValue("copy_files", 0)
@@ -144,23 +108,47 @@ class ViewerMainWindow(MainWindow):
         # else:
         #     self.parent.settings.setValue("volume_mapper", "cpu")
 
-        self.settings.setValue("gpu_size", float(settings_dialog.fw.widgets['gpu_size_field'].value()))
-        self.settings.setValue("vis_size", float(settings_dialog.fw.widgets['vis_size_field'].value()))
+        self.settings.setValue("gpu_size", float(
+            settings_dialog.fw.widgets['gpu_size_field'].value()))
+        self.settings.setValue("vis_size", float(
+            settings_dialog.fw.widgets['vis_size_field'].value()))
 
     def select_image(self, label=None):
         #print("In select image")
-        dialogue = QFileDialog()
-        file = dialogue.getOpenFileName(self, "Load Images")[0]
+        dialog = QFileDialog()
+        file = dialog.getOpenFileName(self, "Load Images")[0]
         file_extension = Path(file).suffix.lower()
+        self.raw_attrs = {}
+        self.hdf5_attrs = {}
+        print("The file ext: ", file_extension)
         if 'tif' in file_extension:
             print('tif ', file)
             file = os.path.dirname(file)
+        if 'raw' in file_extension:
+            raw_dialog = RawInputDialog(self, file)
+            raw_dialog.Ok.clicked.connect(lambda: self._get_raw_attrs_from_dialog(raw_dialog))
+            raw_dialog.exec_()
+        elif file_extension in ['.nxs', '.h5', '.hdf5']:
+            raw_dialog = HDF5InputDialog(self, file)
+            raw_dialog.Ok.clicked.connect(lambda: self._get_hdf5_attrs_from_dialog(raw_dialog))
+            raw_dialog.exec_()
+        
+
         self.input_dataset_file = file
         if label is not None:
             label.setText(os.path.basename(file))
 
-        return file
+        return file, self.raw_attrs, self.hdf5_attrs
 
+    def _get_raw_attrs_from_dialog(self, dialog):
+        print("got the raw attrs")
+        self.raw_attrs = dialog.get_raw_attrs()
+        dialog.close()
+
+    def _get_hdf5_attrs_from_dialog(self, dialog):
+        print("got the hdf5 attrs")
+        self.hdf5_attrs = dialog.get_hdf5_attrs()
+        dialog.close()
 
     def CreateViewerSettingsPanel(self):
         print("make panel")
@@ -186,8 +174,8 @@ class ViewerMainWindow(MainWindow):
         vs_widgets['loaded_image_dims_value'].setVisible(False)
 
         form.addWidget(vs_widgets['loaded_image_dims_value'],
-                    vs_widgets['loaded_image_dims_label'],
-                    'loaded_image_dims')
+                       vs_widgets['loaded_image_dims_label'],
+                       'loaded_image_dims')
 
         vs_widgets['displayed_image_dims_label'] = QLabel()
         vs_widgets['displayed_image_dims_label'].setText(
@@ -199,8 +187,8 @@ class ViewerMainWindow(MainWindow):
         vs_widgets['displayed_image_dims_value'].setVisible(False)
 
         form.addWidget(vs_widgets['displayed_image_dims_value'],
-                    vs_widgets['displayed_image_dims_label'],
-                    'displayed_image_dims')
+                       vs_widgets['displayed_image_dims_label'],
+                       'displayed_image_dims')
 
         vs_widgets['coords_label'] = QLabel()
         vs_widgets['coords_label'].setText("Display viewer coordinates in: ")
@@ -211,18 +199,18 @@ class ViewerMainWindow(MainWindow):
         vs_widgets['coords_combobox'].setEnabled(False)
         vs_widgets['coords_combobox'].currentIndexChanged.connect(
             self.updateCoordinates)
-        form.addWidget(vs_widgets['coords_combobox'], vs_widgets['coords_label'], 'cords_combo')
+        form.addWidget(vs_widgets['coords_combobox'],
+                       vs_widgets['coords_label'], 'cords_combo')
 
         vs_widgets['coords_warning_label'] = QLabel()
         vs_widgets['coords_warning_label'].setText(
             "Warning: These coordinates are approximate.")
         vs_widgets['coords_warning_label'].setVisible(False)
 
-        form.addSpanningWidget(vs_widgets['coords_warning_label'], 'coords_warning')
+        form.addSpanningWidget(
+            vs_widgets['coords_warning_label'], 'coords_warning')
 
         self.visualisation_setting_widgets = vs_widgets
-
-        
 
     def updateCoordinates(self):
         vs_widgets = self.visualisation_setting_widgets
@@ -246,10 +234,6 @@ class ViewerMainWindow(MainWindow):
 
             viewer.updatePipeline()
 
-        
-
-
-
 
 class ViewerSettingsDialog(SettingsDialog):
 
@@ -258,10 +242,11 @@ class ViewerSettingsDialog(SettingsDialog):
 
         fw = self.fw
 
-        copy_files_checkbox = QCheckBox("Allow a copy of the image files to be stored. ")
+        copy_files_checkbox = QCheckBox(
+            "Allow a copy of the image files to be stored. ")
 
         fw.addSpanningWidget(copy_files_checkbox, 'copy_files_checkbox')
-        
+
         vis_size_label = QLabel("Maximum downsampled image size (GB): ")
         vis_size_entry = QDoubleSpinBox()
         vis_size_entry.setMaximum(64.0)
@@ -274,24 +259,125 @@ class ViewerSettingsDialog(SettingsDialog):
 
         fw.addTitle(QLabel("Advanced Settings"), 'adv_settings_title')
 
-        fw.addSpanningWidget(QLabel("Please set the size of your GPU memory."), 'gpu_size_title')
-        
+        fw.addSpanningWidget(
+            QLabel("Please set the size of your GPU memory."), 'gpu_size_title')
+
         gpu_size_label = QLabel("GPU Memory (GB): ")
         gpu_size_entry = QDoubleSpinBox()
-        
+
         gpu_size_entry.setMaximum(64.0)
         gpu_size_entry.setMinimum(0.00)
         gpu_size_entry.setSingleStep(0.01)
         fw.addWidget(gpu_size_entry, gpu_size_label, 'gpu_size')
 
-        #TODO later when we have a 3D renderer - change volume render settings:
+        # TODO later when we have a 3D renderer - change volume render settings:
         # self.gpu_checkbox = QCheckBox("Use GPU for volume render. (Recommended) ")
         # self.gpu_checkbox.setChecked(True) #gpu is default
         # if self.parent.settings.value("volume_mapper") == "cpu":
         #     self.gpu_checkbox.setChecked(False)
-        
+
+
+class RawInputDialog(FormDialog):
+    def __init__(self, parent, fname):
+        super(RawInputDialog, self).__init__(parent, fname)
+        title = "Config for " + os.path.basename(fname)
+        self.setWindowTitle(title)
+        fw = self.formWidget
+
+        # dimensionality:
+        dimensionalityLabel = QLabel("Dimensionality")
+        dimensionalityValue = QComboBox()
+        dimensionalityValue.addItems(["3D", "2D"])
+        dimensionalityValue.setCurrentIndex(0)
+        fw.addWidget(dimensionalityValue,
+                     dimensionalityLabel, "dimensionality")
+
+        validator = QtGui.QIntValidator()
+
+        # Entry for size of dimensions:
+        for dim in ['X', 'Y', 'Z']:
+            ValueEntry = QLineEdit()
+            ValueEntry.setValidator(validator)
+            ValueEntry.setText("0")
+            Label = QLabel("Size {}".format(dim))
+            fw.addWidget(ValueEntry, Label, 'dim_{}'.format(dim))
+
+        dimensionalityValue.currentIndexChanged.connect(
+            lambda: fw.widgets['dim_Z_field'].setEnabled(True)
+            if fw.widgets['dimensionality_field'].currentIndex() == 0 else
+            fw.widgets['dim_Z_field'].setEnabled(False))
+
+        # Data Type
+        dtypeLabel = QLabel("Data Type")
+        dtypeValue = QComboBox()
+        dtypeValue.addItems(["int8", "uint8", "int16", "uint16"])
+        dtypeValue.setCurrentIndex(1)
+        fw.addWidget(dtypeValue, dtypeLabel, 'dtype')
+
+        # Endianness
+        endiannesLabel = QLabel("Byte Ordering")
+        endiannes = QComboBox()
+        endiannes.addItems(["Big Endian", "Little Endian"])
+        endiannes.setCurrentIndex(1)
+        fw.addWidget(endiannes, endiannesLabel, 'endianness')
+
+        # Fortran Ordering
+        fortranLabel = QLabel("Fortran Ordering")
+        fortranOrder = QComboBox()
+        fortranOrder.addItems(["Fortran Order: XYZ", "C Order: ZYX"])
+        fortranOrder.setCurrentIndex(0)
+        fw.addWidget(fortranOrder, fortranLabel, "is_fortran")
+
+        self.setLayout(fw.uiElements['verticalLayout'])
+
+    def get_raw_attrs(self):
+        raw_attrs = {}
+        widgets = self.formWidget.widgets
+        dimensionality = [3, 2][widgets['dimensionality_field'].currentIndex()]
+        dims = []
+        dims.append(int(widgets['dim_X_field'].text()))
+        dims.append(int(widgets['dim_Y_field'].text()))
+        if dimensionality == 3:
+            dims.append(int(widgets['dim_Z_field'].text()))
+
+        raw_attrs['dimensions'] = dims
+        raw_attrs['is_fortran'] = not bool(widgets['is_fortran_field'].currentIndex())
+        raw_attrs['is_big_endian'] = not bool(widgets['endianness_field'].currentIndex())
+        raw_attrs['typecode'] = widgets['dtype_field'].currentText()
+
+        return raw_attrs
+
+class HDF5InputDialog(FormDialog):
+    def __init__(self, parent, fname):
+        super(HDF5InputDialog, self).__init__(parent, fname)
+        title = "Config for " + os.path.basename(fname)
+        self.setWindowTitle(title)
+        fw = self.formWidget
+
+        # dimensionality:
+        datasetLabel = QLabel("Dataset Name")
+        valueEntry = QLineEdit()
+        valueEntry.setText("entry1/tomo_entry/data/data")
+        fw.addWidget(valueEntry, datasetLabel, "dataset_name")
+
+        # is it Acquisition Data?
+        dtypeLabel = QLabel("Acquisition Data: ")
+        dtypeValue = QComboBox()
+        dtypeValue.addItems(["True", "False"])
+        fw.addWidget(dtypeValue, dtypeLabel, 'dtype')
+
+
+    def get_hdf5_attrs(self):
+        hdf5_attrs = {}
+        widgets = self.formWidget.widgets
+
+        hdf5_attrs['dataset_name'] = widgets['dataset_name_field'].text()
+        hdf5_attrs['is_acquisition_data'] = not bool(widgets['dtype_field'].currentIndex())
+
+        return hdf5_attrs
 
 # For running example window -----------------------------------------------------------
+
 
 def create_main_window():
     window = ViewerMainWindow("Test Viewer Main Window", '2D')
@@ -302,6 +388,9 @@ def main():
     app = QApplication(sys.argv)
     window = create_main_window()
     window.show()
+    r = RawInputDialog(window, 'fname')
+    r.show()
+    print(r.get_raw_attrs())
     app.exec_()
 
 
