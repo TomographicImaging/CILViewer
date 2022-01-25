f879a1f6db7688be0f486161eaa05b79192d1c99
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2022-01-25 10:36:57 +0000

    cilNumpyMETAImageWriter tests

--- Wrappers/Python/test/test_conversion.py
+++ Wrappers/Python/test/test_conversion.py
@@ -22,47 +22,124 @@ class TestConversion(unittest.TestCase):
         # Generate random 3D array and write to HDF5:
         np.random.seed(1)
         self.input_3D_array = np.random.randint(10, size=(5, 10, 6), dtype=np.uint8)
-        self.input_3D_array_fortran = np.asfortranarray(np.random.randint(10, size=(5, 10, 6), dtype=np.uint8))
         bytes_3D_array = bytes(self.input_3D_array)
         self.raw_filename_3D = 'test_3D_data.raw'
         with open(self.raw_filename_3D, 'wb') as f:
             f.write(bytes_3D_array)
 
 
-    def test_cilNumpyMETAImageWriter(self):
+    def test_cilNumpyMETAImageWriter_contiguous(self):
         ''' Tests using cilNumpyMETAImageWriter to write
         a numpy array with a metaimage header. Tests a numpy array
-        in contiguous order and in fortran order.'''
+        in contiguous order'''
 
-        # test write contiguous numpy array:
-        self.meta_header_filename_3D = 'test_3D_data.mhd'
+        # Write a numpy array to a file:
         self.numpy_filename_3D = 'test_3D_data.npy'
+        np.save(self.numpy_filename_3D, self.input_3D_array)
+
+        # Read in the numpy array:
+        numpy_reader = cilNumpyResampleReader()
+        # default target size higher than total size so it reads whole array
+        numpy_reader.SetFileName(self.numpy_filename_3D)
+        numpy_reader.Update()
+        read_numpy_as_vtk = numpy_reader.GetOutput()
+        read_numpy = Converter.vtk2numpy(read_numpy_as_vtk)
+
+        # Now we want to save a mhd file to go with it:
         writer = cilNumpyMETAImageWriter()
-        writer.SetInputData(self.input_3D_array)
-        writer.SetFileName('test_3D_data')
-        writer.Write()    
+        writer.SetInputData(read_numpy)
+        writer.SetFileName('test_3D_data_mhd_np')
+        writer.Write()
 
+        # Read the mhd and compare with the numpy array we read in before
+        # First read with the meta image reader:
         reader = vtk.vtkMetaImageReader()
-        reader.SetFileName(self.meta_header_filename_3D)
+        reader.SetFileName('test_3D_data_mhd_np.mhd')
         reader.Update()
-        # An issue here is we have to convert enforcing fortran order:
-        read_mhd = Converter.vtk2numpy(reader.GetOutput(), order='F')
-        np.testing.assert_array_equal(read_mhd, self.input_3D_array)
+        read_mhd_vtk = reader.GetOutput()
+        # all our tests fail if we don't specify order F
+        read_mhd = Converter.vtk2numpy(read_mhd_vtk, order='F')
 
-        # test write fortran numpy array:
-        self.meta_header_filename_3D_fortran = 'test_3D_data_fortran.mhd'
-        self.numpy_filename_3D_fortran = 'test_3D_data_fortran.npy'
+        # This would show the vtkMetaImageReader reads our written mhd fine:
+        # This fails due to wrong ordering
+        # self.assertEqual(read_numpy_as_vtk.GetExtent(), read_mhd_vtk.GetExtent())
+        print("vtkMetaImageReader test passed")
+        np.testing.assert_array_equal(read_numpy, read_mhd)
+        np.testing.assert_array_equal(self.input_3D_array, read_mhd)
+
+        # then read with our reader:
+        reader = cilMetaImageResampleReader()
+        # default target size higher than total size so it reads whole array
+        reader.SetFileName('test_3D_data_mhd_np.mhd')
+        reader.Update()
+        read_mhd_vtk = reader.GetOutput()
+        # all our tests fail if we don't specify order F
+        read_mhd = Converter.vtk2numpy(read_mhd_vtk, order='F')
+
+        # This would show our cilMetaImageResampleReader reads our written mhd fine:
+        # This fails due to wrong ordering:
+        #self.assertEqual(read_numpy_as_vtk.GetExtent(), read_mhd_vtk.GetExtent())
+        print("cilMetaImageResampleReader test passed")
+        np.testing.assert_array_equal(read_numpy, read_mhd)
+        np.testing.assert_array_equal(self.input_3D_array, read_mhd)
+
+
+    def test_cilNumpyMETAImageWriter_fortran(self):
+        ''' Tests using cilNumpyMETAImageWriter to write
+        a numpy array with a metaimage header. Tests a numpy array
+        in fortran order'''
+
+        # Write a numpy array to a file:
+        self.numpy_filename_3D_fortran = 'test_3D_data_F.npy'
+        self.input_3D_array_fortran = np.asfortranarray(self.input_3D_array)
+        np.save(self.numpy_filename_3D_fortran, self.input_3D_array_fortran)
+
+        # Read in the numpy array:
+        numpy_reader = cilNumpyResampleReader()
+        # default target size higher than total size so it reads whole array
+        numpy_reader.SetFileName(self.numpy_filename_3D_fortran)
+        numpy_reader.Update()
+        read_numpy_as_vtk = numpy_reader.GetOutput()
+        read_numpy = Converter.vtk2numpy(read_numpy_as_vtk)
+
+        # Now we want to save a mhd file to go with it:
         writer = cilNumpyMETAImageWriter()
-        writer.SetInputData(self.input_3D_array_fortran)
-        writer.SetFileName('test_3D_data_fortran')
-        writer.Write()    
+        writer.SetInputData(read_numpy)
+        writer.SetFileName('test_3D_data_mhd_np')
+        writer.Write()
 
+        # Read the mhd and compare with the numpy array we read in before
+        # First read with the meta image reader:
         reader = vtk.vtkMetaImageReader()
-        reader.SetFileName(self.meta_header_filename_3D_fortran)
+        reader.SetFileName('test_3D_data_mhd_np.mhd')
         reader.Update()
-        read_mhd_fortran = Converter.vtk2numpy(reader.GetOutput(), order='F')
+        read_mhd_vtk = reader.GetOutput()
+        # all our tests fail if we  specify order F or not
+        read_mhd = Converter.vtk2numpy(read_mhd_vtk, order='F')
 
-        np.testing.assert_array_equal(read_mhd_fortran, self.input_3D_array_fortran)
+        # This would show the vtkMetaImageReader reads our written mhd fine:
+        # This fails due to wrong ordering
+        #self.assertEqual(read_numpy_as_vtk.GetExtent(), read_mhd_vtk.GetExtent())
+        print("vtkMetaImageReader test passed")
+        np.testing.assert_array_equal(read_numpy, read_mhd)
+        np.testing.assert_array_equal(self.input_3D_array, read_mhd)
+
+        # then read with our reader:
+        reader = cilMetaImageResampleReader()
+        # default target size higher than total size so it reads whole array
+        reader.SetFileName('test_3D_data_mhd_np.mhd')
+        reader.Update()
+        read_mhd_vtk = reader.GetOutput()
+        # all our tests fail if we  specify order F or not
+        read_mhd = Converter.vtk2numpy(read_mhd_vtk)
+
+        # This would show our cilMetaImageResampleReader reads our written mhd fine:
+        # This fails due to wrong ordering:
+        #self.assertEqual(read_numpy_as_vtk.GetExtent(), read_mhd_vtk.GetExtent())
+        print("cilMetaImageResampleReader test passed")
+        np.testing.assert_array_equal(read_numpy, read_mhd)
+        np.testing.assert_array_equal(self.numpy_filename_3D_fortran, read_mhd)
+
 
     def test_WriteMETAImageHeader(self):
         '''writes a mhd file to go with a raw 
@@ -71,8 +148,6 @@ class TestConversion(unittest.TestCase):
         by comparing to array read with cilRawResampleReader
         directly from RawResampleReader and the original contents'''
 
-
-        # read raw file's info:
         data_filename = self.raw_filename_3D
         header_filename = 'raw_header.mhd'
         typecode = 'uint8'
@@ -88,6 +163,9 @@ class TestConversion(unittest.TestCase):
         reader.Update()
         read_mhd_raw = Converter.vtk2numpy(reader.GetOutput())
 
+        # Tests the array we read with vtk.vtkMetaImageReader matches original array:
+        np.testing.assert_array_equal(read_mhd_raw, self.input_3D_array)
+
         reader = cilRawResampleReader()
         target_size = int(1e12)
         reader.SetTargetSize(target_size)
@@ -102,8 +180,8 @@ class TestConversion(unittest.TestCase):
         image = reader.GetOutput()
         raw_array = Converter.vtk2numpy(image)
 
-        np.testing.assert_array_equal(read_mhd_raw, raw_array)
-        np.testing.assert_array_equal(read_mhd_raw, self.input_3D_array)
+        # Tests the array we read with cilRawResampleReader matches original array:
+        np.testing.assert_array_equal(raw_array, self.input_3D_array)
 
 
     def tearDown(self):
