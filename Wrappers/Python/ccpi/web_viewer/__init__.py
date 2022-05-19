a1c071ccb7060febae6c77c53dcdbaaf60c06eb0
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-05-19 15:29:37 +0100

    Update copyright notices Apache 2.0 to include UKRI full name

--- Wrappers/Python/ccpi/web_viewer/__init__.py
+++ Wrappers/Python/ccpi/web_viewer/__init__.py
@@ -1,6 +1,7 @@
 #
+#   Copyright 2022 STFC, United Kingdom Research and Innovation
+#
 #   Author 2022 Samuel Jones
-#   Copyright 2022 SCD Rutherford Appleton Laboratory UKRI
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
