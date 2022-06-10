48e909fb63322b8c92cc548761d93ff498f1e97d
Samuel Jones <samuel.jones@stfc.ac.uk>
2022-06-10 09:51:45 +0100

    Initial pass at a docker container for trame

---
+++ docker/web-app/clone-and-install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+git clone --depth 0 https://github.com/vais-ral/CILViewer
+
+. /home/abc/mambaforge/etc/profile.d/conda.sh
+conda activate cilviewer_webapp
+pip install CILViewer/Wrappers/Python
+
+rm -rf CILViewer
---
+++ docker/web-app/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+. /home/abc/mambaforge/etc/profile.d/conda.sh
+conda activate cilviewer_webapp
+
+cilwebapp ./data
