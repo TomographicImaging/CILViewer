4910c6499e02262c315fe29da7a9cfb629ca1946
Samuel Jones <samjones714@gmail.com>
2022-06-17 14:20:46 +0100

    Update web_app.py

--- Wrappers/Python/ccpi/web_viewer/web_app.py
+++ Wrappers/Python/ccpi/web_viewer/web_app.py
@@ -15,9 +15,6 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
-import faulthandler
-
-faulthandler.enable()
 import os
 import sys
 
