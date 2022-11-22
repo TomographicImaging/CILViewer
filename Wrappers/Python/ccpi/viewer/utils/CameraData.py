de20a82b5f5e51be1cfa0f6f5eef794d9fd39096
lauramurgatroyd <laura.murgatroyd@stfc.ac.uk>
2022-11-22 12:55:54 +0000

    rename camera data

---
+++ Wrappers/Python/ccpi/viewer/utils/CameraData.py
@@ -0,0 +1,39 @@
+#
+#   Copyright 2022 STFC, United Kingdom Research and Innovation
+#
+#   Author 2022 Samuel Jones, Laura Murgatroyd
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+from dataclasses import dataclass
+
+import vtk
+
+
+@dataclass(init=False)
+class CameraData:
+    position: list
+    focalPoint: list
+    viewUp: list
+
+    def __init__(self, camera: vtk.vtkCamera):
+        self.position = camera.GetPosition()
+        self.focalPoint = camera.GetFocalPoint()
+        self.viewUp = camera.GetViewUp()
+
+    @staticmethod
+    def CopyDataToCamera(camera_data, vtkcamera):
+        vtkcamera.SetPosition(*camera_data.position)
+        vtkcamera.SetFocalPoint(*camera_data.focalPoint)
+        vtkcamera.SetViewUp(*camera_data.viewUp)
