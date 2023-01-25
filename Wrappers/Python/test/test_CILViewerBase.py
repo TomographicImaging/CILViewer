bc5ea6f809662605655b665c68f4dc6591685740
Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
2023-01-25 14:53:11 +0000

    Update licence text

--- Wrappers/Python/test/test_CILViewerBase.py
+++ Wrappers/Python/test/test_CILViewerBase.py
@@ -1,6 +1,6 @@
-#   Copyright 2022 STFC, United Kingdom Research and Innovation
+#   Copyright 2023 STFC, United Kingdom Research and Innovation
 #
-#   Author 2022 Samuel Jones
+#   Author 2023 Laura Murgatroyd
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
