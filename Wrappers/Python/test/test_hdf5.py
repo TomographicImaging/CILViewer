283e9f02ec8189226ec954c96018013968d396bf
Edoardo Pasca <edo.paskino@gmail.com>
2023-10-22 10:10:59 +0200

    Standalone viewer app defaults to downsample on Z (#362)
    * closes Downsampling should use round rather than int #359
    * update unittest and remove temporary test files
    * use pytest in action
    * move functionality to utils.conversion

--- Wrappers/Python/test/test_hdf5.py
+++ Wrappers/Python/test/test_hdf5.py
@@ -4,21 +4,8 @@ import unittest
 import h5py
 import numpy as np
 import vtk
-from ccpi.viewer.utils.conversion import Converter, cilHDF5ResampleReader, cilHDF5CroppedReader
-from ccpi.viewer.utils.hdf5_io import (HDF5Reader, HDF5SubsetReader, write_image_data_to_hdf5)
-
-
-def calculate_target_downsample_shape(max_size, total_size, shape, acq=False):
-    if not acq:
-        xy_axes_magnification = np.power(max_size / total_size, 1 / 3)
-        slice_per_chunk = int(1 / xy_axes_magnification)
-    else:
-        slice_per_chunk = 1
-        xy_axes_magnification = np.power(max_size / total_size, 1 / 2)
-    num_chunks = 1 + len([i for i in range(slice_per_chunk, shape[2], slice_per_chunk)])
-
-    target_image_shape = (int(xy_axes_magnification * shape[0]), int(xy_axes_magnification * shape[1]), num_chunks)
-    return target_image_shape
+from ccpi.viewer.utils.conversion import Converter, calculate_target_downsample_shape, cilHDF5CroppedReader, cilHDF5ResampleReader
+from ccpi.viewer.utils.hdf5_io import (HDF5Reader, HDF5SubsetReader, write_image_data_to_hdf5)      
 
 
 class TestHDF5IO(unittest.TestCase):
--- Wrappers/Python/test/test_resample_readers.py
+++ Wrappers/Python/test/test_resample_readers.py
@@ -4,21 +4,11 @@ import warnings
 
 import numpy as np
 import vtk
-from ccpi.viewer.utils.conversion import (Converter, cilMetaImageResampleReader, cilNumpyMETAImageWriter,
-                                          cilNumpyResampleReader, cilRawResampleReader, cilTIFFResampleReader)
+from ccpi.viewer.utils.conversion import Converter, calculate_target_downsample_shape, \
+    cilRawResampleReader, cilTIFFResampleReader, cilNumpyMETAImageWriter, cilMetaImageResampleReader,\
+    cilNumpyResampleReader
 
 
-def calculate_target_downsample_shape(max_size, total_size, shape, acq=False):
-    if not acq:
-        xy_axes_magnification = np.power(max_size / total_size, 1 / 3)
-        slice_per_chunk = int(1 / xy_axes_magnification)
-    else:
-        slice_per_chunk = 1
-        xy_axes_magnification = np.power(max_size / total_size, 1 / 2)
-    num_chunks = 1 + len([i for i in range(slice_per_chunk, shape[2], slice_per_chunk)])
-
-    target_image_shape = (int(xy_axes_magnification * shape[0]), int(xy_axes_magnification * shape[1]), num_chunks)
-    return target_image_shape
 
 
 class TestResampleReaders(unittest.TestCase):
