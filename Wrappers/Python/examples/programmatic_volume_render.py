32d08e890236bb88dcb5916949d003ade24af760
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-06-28 10:24:24 +0100

    updates to script

--- Wrappers/Python/examples/programmatic_volume_render.py
+++ Wrappers/Python/examples/programmatic_volume_render.py
@@ -6,7 +6,8 @@ import numpy as np
 
 def createAnimation(viewer, FrameCount=20, 
                     InitialCameraPosition=None, FocalPoint=None, 
-                    ClippingRange=None, AngleRange = 360, ViewUp = None):
+                    ClippingRange=None, AngleRange = 360, ViewUp = None, clip_plane=False, 
+                    fname_offset=0, fname_prefix='test', output_dir='.'):
     
     viewer
 
@@ -40,16 +41,28 @@ def createAnimation(viewer, FrameCount=20,
     r = np.sqrt(((InitialCameraPosition[0]-FocalPoint[0])**2)
     +(InitialCameraPosition[1]-FocalPoint[1])**2)
     print('Radius (distance from camera to focal point): {}'.format(r))
-    
 
-
+    if not clip_plane:
+        viewer.style.ToggleSliceVisibility()
+        
     #Animating the camera
     for x in range(FrameCount):
-        # move the slice during rotation
-        new_slice = round(x/FrameCount * viewer.img3D.GetDimensions()[2])
-        print('displaying slice {}'.format(new_slice))
-        viewer.style.SetActiveSlice(new_slice)
-        viewer.updatePipeline(False)
+        if clip_plane:
+            # move the slice during rotation
+            new_slice = round(x/FrameCount * viewer.img3D.GetDimensions()[2])
+            print('displaying slice {}'.format(new_slice))
+            viewer.style.SetActiveSlice(new_slice)
+            viewer.updatePipeline(False)
+            # plane on the slice plane
+            plane = vtk.vtkPlane()
+            plane.SetOrigin(0,0,new_slice * viewer.img3D.GetSpacing()[2])
+            plane.SetNormal(0,0,-1)
+
+            viewer.volume.GetMapper().RemoveAllClippingPlanes()
+            viewer.volume.GetMapper().AddClippingPlane(plane)
+            viewer.volume.Modified()
+            viewer.getRenderer().Render()
+            
 
         
         angle = (2 * np.pi ) * (x/FrameCount)
@@ -73,7 +86,7 @@ def createAnimation(viewer, FrameCount=20,
         #Rendering and saving the render
         viewer.getRenderer().Render()
         viewer.renWin.Render()
-        saveRender(viewer, x, 'test')
+        saveRender(viewer, x + fname_offset, fname_prefix, directory=output_dir)
 
 def saveRender(viewer, number, file_prefix, directory='.'):
     w2if = vtk.vtkWindowToImageFilter()
@@ -88,6 +101,7 @@ def saveRender(viewer, number, file_prefix, directory='.'):
     writer.SetInputConnection(w2if.GetOutputPort())
     writer.Write()
 
+
 if __name__ == '__main__':
     v = viewer3D(debug=False)
     from ccpi.viewer.utils.io import ImageReader, cilHDF5ResampleReader, cilTIFFResampleReader
@@ -96,10 +110,22 @@ if __name__ == '__main__':
     # reader = ImageReader(r"C:\Users\ofn77899\Data\dvc/frame_000_f.npy", resample=False)
     # reader = ImageReader(r"{}/head_uncompressed.mha".format(os.path.dirname(__file__)), resample=False)
     # data = reader.Read()
+
+    pokemon = 'Picachu'
+    subdir = 'PicachuFull'
+    # subdir = 'Not_Angled'
+
     
-    dirname = r"C:\Users\ofn77899\Data\HOW\Not_Angled"
+    dirname = os.path.abspath("C:/Users/ofn77899/Data\HOW/{}/{}/".format(pokemon, subdir))
+    
     fnames = [el for el in glob.glob(os.path.join(dirname, "*.tiff"))]
     
+    print ("#########################")
+    print (dirname, fnames)
+
+    if len(fnames) == 0:
+        exit(1)
+
     reader = cilTIFFResampleReader()
     reader.SetFileName(fnames)
     reader.SetTargetSize(1024*1024*1024)
@@ -109,6 +135,8 @@ if __name__ == '__main__':
     dims = data.GetDimensions()
     spac = list(data.GetSpacing())
     spac[2] = dims[0]/dims[2]
+    spac[2] *= 0.75
+
     print (data.GetSpacing())
     data.SetSpacing(*spac)
     print (data.GetSpacing())
@@ -117,30 +145,26 @@ if __name__ == '__main__':
 
     v.style.ToggleVolumeVisibility()
     
-    color_percentiles = (75., 99.)
-    scalar_opacity_percentiles = (80., 99.)
-    gradient_opacity_percentiles = (95., 99.9)
-    max_opacity = 0.05
-    
 
-    # self.setVolumeColorPercentiles(*color_percentiles, update_pipeline=False)
-    # self.setScalarOpacityPercentiles(*scalar_opacity_percentiles, update_pipeline=False)
-    # self.setGradientOpacityPercentiles(*gradient_opacity_percentiles, update_pipeline=False)
-    # self.setMaximumOpacity(max_opacity)
-
-    v.setVolumeColorMapName('bone')
+    v.setVolumeColorMapName('viridis')
 
     # define colors and opacity with default values
     colors, opacity = v.getColorOpacityForVolumeRender()
 
     v.volume_property.SetColor(colors)
 
-    method = 'gradient'
+    method = 'scalar'
     if method == 'gradient':
+        color_percentiles = (65., 98.)
+        gradient_opacity_percentiles = (75., 99.9)
+        max_opacity = 0.1
         v.setVolumeRenderOpacityMethod('gradient')
         v.setGradientOpacityPercentiles(*gradient_opacity_percentiles, update_pipeline=False)
         v.volume_property.SetGradientOpacity(opacity)
     else:
+        color_percentiles = (75., 99.)
+        scalar_opacity_percentiles = (94., 99.5)
+        max_opacity = 0.1
         v.setVolumeRenderOpacityMethod('scalar')
         v.setScalarOpacityPercentiles(*scalar_opacity_percentiles, update_pipeline=False)
         v.volume_property.SetScalarOpacity(opacity)
@@ -148,14 +172,30 @@ if __name__ == '__main__':
     v.setVolumeColorPercentiles(*color_percentiles, update_pipeline=False)
     v.setMaximumOpacity(max_opacity)
 
-    # v.style.ToggleSliceVisibility()
-
     # default background
     # self.ren.SetBackground(.1, .2, .4)
     v.ren.SetBackground(0, 0, 0)
+    # v.ren.SetBackground(1,1,1)
     
-    # v.startRenderLoop()
-
-    createAnimation(v, FrameCount=10, InitialCameraPosition=((483.8653687626969, -2173.282759469902, 1052.4208133258792)), AngleRange=360)
+    
+    createAnimation(v, FrameCount=100, 
+                    InitialCameraPosition=(
+                        (483.8653687626969, -2173.282759469902, 1052.4208133258792)
+                        ), 
+                    AngleRange=360, 
+                    clip_plane=True, 
+                    fname_offset=0,
+                    fname_prefix=pokemon,
+                    output_dir="C:/Users/ofn77899/Data/HOW/Picachu/renders")
+    
+    createAnimation(v, FrameCount=100, 
+                    InitialCameraPosition=(
+                        (483.8653687626969, -2173.282759469902, 1052.4208133258792)
+                        ), 
+                    AngleRange=360, 
+                    clip_plane=False, 
+                    fname_offset=100,
+                    fname_prefix=pokemon,
+                    output_dir=r'C:\Users\ofn77899\Data\HOW\Picachu\renders')
 
     
