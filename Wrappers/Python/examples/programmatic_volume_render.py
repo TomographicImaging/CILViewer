10e0dcbb3fdcb23b2acd0d165c89c9377fb9413c
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2024-07-01 11:42:18 +0100

    added argparser

--- Wrappers/Python/examples/programmatic_volume_render.py
+++ Wrappers/Python/examples/programmatic_volume_render.py
@@ -1,8 +1,16 @@
-from ccpi.viewer.CILViewer2D import CILViewer2D as viewer2D
 from ccpi.viewer.CILViewer import CILViewer as viewer3D
 import glob
 import vtk
 import numpy as np
+from ccpi.viewer.utils.io import ImageReader, \
+                                cilTIFFResampleReader
+from ccpi.viewer.utils.conversion import cilNumpyResampleReader, \
+                                         cilHDF5ResampleReader,\
+                                         cilRawResampleReader
+
+import os, sys
+import argparse
+
 
 def createAnimation(viewer, FrameCount=20, 
                     InitialCameraPosition=None, FocalPoint=None, 
@@ -101,44 +109,150 @@ def saveRender(viewer, number, file_prefix, directory='.'):
     writer.SetInputConnection(w2if.GetOutputPort())
     writer.Write()
 
+class Readers():
+    @staticmethod
+    def get_reader(filename, dimensions=None, data_path=None):
+        if os.path.isdir(filename):
+            # should return and TIFF reader
+            return Readers._return_tiff_reader(filename)
+        else:
+            if data_path is not None:
+                
+                # HDF5 file
+                import h5py
+                with h5py.File(filename, 'r') as f:
+                    f.items()
+                reader = cilHDF5ResampleReader()
+                reader.SetFileName(filename)
+                reader.SetDatasetName(data_path)
+                return reader
+            
+            elif dimensions is not None:
+                reader = cilRawResampleReader()
+                reader.SetFileName(filename)
+                reader.SetStoredArrayShape(dimensions)
+                return reader
+            else:
+                # Figure out what file we have
+                # 2) npy
+                from ccpi.viewer.utils.conversion import parseNpyHeader
+                try:
+                    header = parseNpyHeader(filename)
+                    if header['type'] == 'NUMPY':
+                        ret = cilNumpyResampleReader()
+                        ret.SetFileName(filename)
+                        return ret
+                except Exception as err:
+                    pass
+                
+                # 3) mha/mhd
+                try:
+                    from ccpi.viewer.utils.conversion import cilMetaImageResampleReader
+                    reader = cilMetaImageResampleReader()
+                    reader.SetFileName(filename)
+                    
+                    reader.ReadMetaImageHeader()
+                    return reader
+                except Exception as err:
+                        pass
+                
+            
+            # 1) raw file
+            
+    @staticmethod
+    def _return_tiff_reader(dirname):            
+        fnames = [el for el in glob.glob(os.path.join(dirname, "*.tiff"))]
+    
+        print ("#########################") 
+        print (dirname, fnames)
 
-if __name__ == '__main__':
-    v = viewer3D(debug=False)
+        if len(fnames) == 0:
+            # try again with .tif extension
+            fnames = [el for el in glob.glob(os.path.join(dirname, "*.tiff"))]
+            raise ValueError(f"Directory {dirname} does not contain tiff files with extension tiff or tif")
+        reader = cilTIFFResampleReader()
+        reader.SetFileName(fnames)
+        return reader
+    
+
+if __name__ == '__main__':    
+    parser = argparse.ArgumentParser(description='Create awesome volume render')
+
+    parser.add_argument('--verbose', type=int, default=0)
+    parser.add_argument('--volume', type=str, default='scalar')
+    parser.add_argument('--max_opacity', type=float, default=0.1)
+    parser.add_argument('--spacing', type=float, nargs=3, default=None)
+    parser.add_argument('--spacing_multiplier', type=float, nargs=3, default=None)
+    parser.add_argument('--max_size', type=int, nargs=3, default=[1024,1024,1024])
+    parser.add_argument('--resample_z', type=bool, default=True)
+    parser.add_argument('--colormap', type=str, default='viridis')
+    parser.add_argument('--num_frames', type=int, default=10)
+    parser.add_argument('--output_dir', type=str, default='.')
+    parser.add_argument('--output_prefix', type=str, default='awesome')
+    parser.add_argument('--dimensions', type=int, nargs=3, default=None)
+    parser.add_argument('--data_path', type=str, default=None)
+    parser.add_argument('--camera_position', type=float)
+    
+    parser.add_argument('input', help='File or directory')
+    args = parser.parse_args()
+
+    print (args)
+
+    # print ("################### ", len(sys.argv))
+    # exit(0)
+
+    from ccpi.viewer.CILViewer import CILViewer as viewer3D
     from ccpi.viewer.utils.io import ImageReader, cilHDF5ResampleReader, cilTIFFResampleReader
-    from ccpi.viewer.utils.conversion import cilTIFFImageReaderInterface
-    import os
+    import vtk
+
+    v = viewer3D(debug=True if args.verbose >0 else False)
+
+
+
+    # detect the file format and return the appropriate reader
+
+
     # reader = ImageReader(r"C:\Users\ofn77899\Data\dvc/frame_000_f.npy", resample=False)
     # reader = ImageReader(r"{}/head_uncompressed.mha".format(os.path.dirname(__file__)), resample=False)
     # data = reader.Read()
 
-    pokemon = 'Picachu'
-    # subdir = 'PicachuFull'
-    subdir = 'Not_Angled'
 
-    
-    dirname = os.path.abspath("C:/Users/ofn77899/Data\HOW/{}/{}/".format(pokemon, subdir))
-    
-    fnames = [el for el in glob.glob(os.path.join(dirname, "*.tiff"))]
-    
-    print ("#########################")
-    print (dirname, fnames)
 
-    if len(fnames) == 0:
-        exit(1)
+    pokemon = args.output_prefix
+    
+    # subdir = 'Not_Angled'
 
-    reader = cilTIFFResampleReader()
-    reader.SetFileName(fnames)
+    reader = Readers.get_reader(args.input, dimensions=args.dimensions, 
+                                data_path=args.data_path)
+    
+    # dirname = os.path.abspath("C:/Users/ofn77899/Data\HOW/{}/{}/".format(pokemon, subdir))
+    
+    # fnames = [el for el in glob.glob(os.path.join(dirname, "*.tiff"))]
+    
+    # print ("#########################") 
+    # print (dirname, fnames)
+
+    # if len(fnames) == 0:
+    #     exit(1)
+
+    # reader = cilTIFFResampleReader()
+    # reader.SetFileName(fnames)
     reader.SetTargetSize(1024*1024*1024)
     reader.Update()
     data = reader.GetOutput()
 
-    dims = data.GetDimensions()
-    spac = list(data.GetSpacing())
-    spac[2] = dims[0]/dims[2]
-    spac[2] *= 0.75
-
     print (data.GetSpacing())
-    data.SetSpacing(*spac)
+        
+    if args.spacing is not None:
+        data.SetSpacing(*args.spacing)
+    elif args.spacing_multiplier is not None:
+        dims = data.GetDimensions()
+        spac = list(data.GetSpacing())
+        # spac[2] = dims[0]/dims[2]
+        # spac[2] *= 0.75
+        for i in range(3):
+            spac[i] *= args.spacing_multiplier[i] 
+        data.SetSpacing(*spac)
     print (data.GetSpacing())
 
     v.setInputData(reader.GetOutput())
@@ -178,7 +292,7 @@ if __name__ == '__main__':
     v.ren.SetBackground(1,1,1)
     
     
-    createAnimation(v, FrameCount=100, 
+    createAnimation(v, FrameCount=args.num_frames, 
                     InitialCameraPosition=(
                         (483.8653687626969, -2173.282759469902, 1052.4208133258792)
                         ), 
@@ -188,7 +302,7 @@ if __name__ == '__main__':
                     fname_prefix=pokemon,
                     output_dir="C:/Users/ofn77899/Data/HOW/Picachu/renders")
     
-    createAnimation(v, FrameCount=100, 
+    createAnimation(v, FrameCount=args.num_frames, 
                     InitialCameraPosition=(
                         (483.8653687626969, -2173.282759469902, 1052.4208133258792)
                         ), 
