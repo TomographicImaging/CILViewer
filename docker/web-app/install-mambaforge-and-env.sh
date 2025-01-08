5b194fa1cadeb995e7735cef501842389129ef19
Laura Murgatroyd <60604372+lauramurgatroyd@users.noreply.github.com>
2025-01-08 15:10:48 +0000

    rename mamabaforge to miniforge

--- docker/web-app/install-mambaforge-and-env.sh
+++ docker/web-app/install-mambaforge-and-env.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && 
 INSTALL_DIR=$HOME/mambaforge
 
 cd $HOME
-curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-x86_64.sh
-bash $HOME/Mambaforge-Linux-x86_64.sh -b -p $INSTALL_DIR
-rm $HOME/Mambaforge-Linux-x86_64.sh
+curl -L -O https://github.com/conda-forge/miniforge/releases/download/24.11.2-1/Miniforge3-Linux-x86_64.sh
+bash $HOME/Miniforge3-Linux-x86_64.sh -b -p $INSTALL_DIR
+rm $HOME/Miniforge3-Linux-x86_64.sh
 . $INSTALL_DIR/etc/profile.d/conda.sh
 mamba env create -f $SCRIPT_DIR/environment.yml
 conda activate cilviewer_webapp
