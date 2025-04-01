b706a57c36352dac1b958c3272d25a1e823308f7
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-04-01 08:38:58 +0100

    add min example

---
+++ Wrappers/Python/examples/ui_examples/qmin_example.py
@@ -0,0 +1,78 @@
+# coding=utf-8
+
+import sys
+
+from vtkmodules.vtkFiltersSources import vtkConeSource
+from vtkmodules.vtkRenderingCore import vtkActor, vtkPolyDataMapper, vtkRenderer
+# load implementations for rendering and interaction factory classes
+import vtkmodules.vtkRenderingOpenGL2
+import vtkmodules.vtkInteractionStyle
+from vtk.qt import QVTKRenderWindowInteractor as QVTK
+
+# import QVTKRenderWindowInteractor as QVTK
+QVTKRenderWindowInteractor = QVTK.QVTKRenderWindowInteractor
+
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QApplication, QMainWindow
+# if QVTK.PyQtImpl == 'PySide6':
+#     from PySide6.QtCore import Qt
+#     from PySide6.QtWidgets import QApplication, QMainWindow
+# elif QVTK.PyQtImpl == 'PySide2':
+#     from PySide2.QtCore import Qt
+#     from PySide2.QtWidgets import QApplication, QMainWindow
+# else:
+#     from PySide.QtCore import Qt
+#     from PySide.QtGui import QApplication, QMainWindow
+
+
+def QVTKRenderWidgetConeExample(argv):
+    """A simple example that uses the QVTKRenderWindowInteractor class."""
+    # every QT app needs an app
+    app = QApplication(['QVTKRenderWindowInteractor'])
+
+    window = QMainWindow()
+
+    # create the widget
+    widget = QVTKRenderWindowInteractor(window)
+    window.setCentralWidget(widget)
+    # if you don't want the 'q' key to exit comment this.
+    widget.AddObserver("ExitEvent", lambda o, e, a=app: a.quit())
+
+    ren = vtkRenderer()
+    widget.GetRenderWindow().AddRenderer(ren)
+
+    print("##### ", widget.GetRenderWindow().GetSize())
+
+    widget.GetRenderWindow().SetSize(500,800)
+
+    cone = vtkConeSource()
+    cone.SetResolution(8)
+
+    coneMapper = vtkPolyDataMapper()
+    coneMapper.SetInputConnection(cone.GetOutputPort())
+
+    coneActor = vtkActor()
+    coneActor.SetMapper(coneMapper)
+
+    ren.AddActor(coneActor)
+
+    # show the widget
+    window.show()
+
+    widget.Initialize()
+    widget.GetRenderWindow().SetSize(500,800)
+    print("##### ", widget.GetRenderWindow().GetSize())
+
+    widget.Start()
+
+    # start event processing
+    # Source: https://doc.qt.io/qtforpython/porting_from2.html
+    # 'exec_' is deprecated and will be removed in the future.
+    # Use 'exec' instead.
+    try:
+        app.exec()
+    except AttributeError:
+        app.exec_()
+
+if __name__ == "__main__":
+    QVTKRenderWidgetConeExample(sys.argv)
