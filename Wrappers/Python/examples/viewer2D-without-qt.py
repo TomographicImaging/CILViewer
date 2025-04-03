be263afc9351eeb97ae00c7fb219095484752fcc
Edoardo Pasca <14138589+paskino@users.noreply.github.com>
2025-04-03 16:30:46 +0100

    reset original input

--- Wrappers/Python/examples/viewer2D-without-qt.py
+++ Wrappers/Python/examples/viewer2D-without-qt.py
@@ -1,14 +1,9 @@
 from ccpi.viewer import viewer2D
 from ccpi.viewer.utils import example_data
 
-from ccpi.viewer.utils.conversion import Converter
-import numpy as np
-
-ndata = np.load("/Users/edoardo.pasca/Data/DVC_test_images/frame_010_f.npy")
-data = Converter.numpy2vtkImage(ndata)
 
 v = viewer2D(debug=False)
 # Load head data
-# data = example_data.HEAD.get()
+data = example_data.HEAD.get()
 v.setInputData(data)
 v.startRenderLoop()
